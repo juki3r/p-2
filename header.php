37801557b955a0912ddb55a9e8ca8611f43eb6a2
juki3r <deepong25@gmail.com>
2024-11-11 20:32:29 +0800

    modified:   header.php

--- header.php
+++ header.php
@@ -29,6 +29,17 @@
 </nav>
 
 <script>
-  var active = window.location.pathname;
-  $(".nav a[href|='" + active + "']").parent().addClass("active");
+   $(function () {
+  // this will get the full URL at the address bar
+  var url = window.location.href;
+  // passes on every "a" tag
+  $(".navbar-nav .nav-link").each(function () {
+    // checks if its the same on the address bar
+    if (url == (this.href)) {
+      $(this).closest("li").addClass("active");
+      //for making parent of submenu active
+      $(this).closest("li").parent().parent().addClass("active");
+    }
+  });
+});
 </script>
