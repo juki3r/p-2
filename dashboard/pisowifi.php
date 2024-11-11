3ed0a7aef443f00ede8368dd5ee679c05a25d450
juki3r <deepong25@gmail.com>
2024-11-12 01:05:55 +0800

    modified:   dashboard/pisowifi.php

--- dashboard/pisowifi.php
+++ dashboard/pisowifi.php
@@ -51,7 +51,7 @@ if(isset($_SESSION['id'])){
                         <div class="card-header">
                             <h3 class="px-1">
                                 PISOWIFI
-                                <a href="expenses.php" class="btn btn-primary btn-sm float-end px-2">Next</a>
+                                <a href="expenses.php" class="btn btn-primary btn-sm float-end mx-2">Next</a>
                                 <a href="index.php" class="btn btn-primary btn-sm float-end">Back</a>
                                 
                             </h3>
